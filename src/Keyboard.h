d59165f4de0a8e8d41321773c776d1bc9e85d46f
Vasundhara <vasundharajain1990@gmail.com>
2025-02-24 19:45:27 +0100

    Improve keyboard display

--- src/Keyboard.h
+++ src/Keyboard.h
@@ -4,25 +4,26 @@
 #include <iostream>
 struct Keyboard {
   Keyboard() = default;
-  void display(const std::bitset<26>& invalid_grid_letters,
-               const std::bitset<26>& invalid_cell_letters) {
+  void display(
+      const std::bitset<26>& invalid_grid_letters,
+      const std::bitset<26>& invalid_cell_letters = std::bitset<26>()) const {
     const std::bitset<26>& invalid_letters =
         invalid_grid_letters | invalid_cell_letters;
 
-    auto getchar = [&invalid_letters](int index) {
+    auto getchar = [&invalid_letters](int index) -> char {
       return invalid_letters[index] ? ' ' : 'A' + index;
     };
 
-    std::cout << "\n---------------------------------\n  ";
+    std::cout << "\n----------------------\n";
     std::cout << "| ";
     for (int i = 0; i < 9; i++) {
       std::cout << " " << getchar(i);
     }
-    std::cout << " |\n| ";
+    std::cout << " |\n|  ";
     for (int i = 9; i < 17; i++) {
       std::cout << " " << getchar(i);
     }
-    std::cout << " |\n| ";
+    std::cout << "  |\n| ";
     for (int i = 17; i < 26; i++) {
       std::cout << " " << getchar(i);
     }
