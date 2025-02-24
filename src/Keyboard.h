43d19bf9e839dcb289d5eb01fc87a0307dfbbbcc
Vasundhara <vasundharajain1990@gmail.com>
2025-02-24 09:11:58 +0100

    Add keyboard struct

---
+++ src/Keyboard.h
@@ -0,0 +1,33 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+#include <bitset>
+#include <iostream>
+struct Keyboard {
+  Keyboard() = default;
+  void display(const std::bitset<26>& invalid_grid_letters,
+               const std::bitset<26>& invalid_cell_letters) {
+    const std::bitset<26>& invalid_letters =
+        invalid_grid_letters | invalid_cell_letters;
+
+    auto getchar = [&invalid_letters](int index) {
+      return invalid_letters[index] ? ' ' : 'A' + index;
+    };
+
+    std::cout << "\n---------------------------------\n  ";
+    std::cout << "| ";
+    for (int i = 0; i < 9; i++) {
+      std::cout << " " << getchar(i);
+    }
+    std::cout << " |\n| ";
+    for (int i = 9; i < 17; i++) {
+      std::cout << " " << getchar(i);
+    }
+    std::cout << " |\n| ";
+    for (int i = 17; i < 26; i++) {
+      std::cout << " " << getchar(i);
+    }
+    std::cout << " |\n";
+  }
+};
+
+#endif  // KEYBOARD_H
